7eb264628edbfd21e6055e545f9a1f32e2acc337
Norix <135404437+Veichi@users.noreply.github.com>
2026-06-24 22:00:02 -0400

    Clarify amplitude and frequency in update function
    Refactor update function to clarify amplitude and frequency settings for sine and cosine lines.

--- AnimatedSinCosWav.py
+++ AnimatedSinCosWav.py
@@ -15,8 +15,8 @@ ax.set_yticks(np.arange(-5,6,1))  # Set y-axis ticks
 ax.set_xticks(np.arange(0, 6*np.pi + np.pi/2, np.pi/2)) # Set x-axis ticks
 ax.set_xticklabels(['0', 'π/2', 'π', '3π/2', '2π', '5π/2', '3π', '7π/2', '4π', '9π/2', '5π', '11π/2', '6π', '13π/2'], color='white')  # Set x-axis tick labels and color
 def update(frame):
-    sin_line.set_ydata(1*np.sin(x + frame/10)) 
-    cos_line.set_ydata(1*np.cos(x + frame/10))
+    sin_line.set_ydata(1*np.sin(x + frame/10)) # Set Amp and Freq
+    cos_line.set_ydata(1*np.cos(x + frame/10)) # Set Amp and Freq
     return sin_line, cos_line
 ani = FuncAnimation(fig, update, frames=10000, interval=20)
 ax.set_title('Animated Sine and Cosine Waves', color='purple')  # Set title and color
